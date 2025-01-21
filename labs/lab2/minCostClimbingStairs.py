d6e06d9161d524c2d726c19d5d8a06df4458a9c3
Alex Pison <pisonalex@gmail.com>
2025-01-21 17:24:20 +0100

    minCostClimbingStairs implemented using dynamic programming

---
+++ labs/lab2/minCostClimbingStairs.py
@@ -0,0 +1,14 @@
+def minCostClimbingStairs(stairs):
+    if len(stairs) < 2:
+        return stairs[-1]
+
+    dp = [0 for _ in stairs]
+    dp[0], dp[1] = stairs[0], stairs[1]
+
+    for i in range(2, len(stairs)):
+        dp[i] = min(dp[i - 1], dp[i - 2]) + stairs[i]
+    return dp[-1]
+
+
+if __name__ == "__main__":
+    print(minCostClimbingStairs([1, 101]))
