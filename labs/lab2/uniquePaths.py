26991876d7a474f33d15bbc38fe7549b28e09ebf
Alex Pison <pisonalex@gmail.com>
2025-01-22 10:50:24 +0100

    Unique path rewritten into two solutions - using combinatorics and dynamic programming

--- labs/lab2/uniquePaths.py
+++
@@ -1,9 +0,0 @@
-from math import factorial
-
-
-def uniquePaths(m, n):
-    return factorial(m + n - 2) // (factorial(m - 1) * factorial(n - 1))
-
-
-if __name__ == "__main__":
-    print(uniquePaths(2, 2))
