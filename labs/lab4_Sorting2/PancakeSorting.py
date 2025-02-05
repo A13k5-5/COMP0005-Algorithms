e06afde52dd4f5a54e84573b68d35369d4e2b00f
A13k5-5 <pisonalex@gmail.com>
2025-02-05 11:05:25 +0100

    Pancake sort finding the least number of flips needed using bfs

--- labs/lab4_Sorting2/PancakeSorting.py
+++ labs/lab4_Sorting2/PancakeSorting.py
@@ -1,5 +1,8 @@
+from collections import deque
+
+
 def flip(a, i, flips):
-    if i == 0:
+    if i <= 0:
         return
     flips = flips.append(i)
     m, n = 0, i
@@ -7,7 +10,13 @@ def flip(a, i, flips):
         a[m], a[n] = a[n], a[m]
         m += 1
         n -= 1
-    print(a)
+    # print(a)
+
+
+def flipTuple(a, i):
+    flips = list(a)
+    flip(flips, i, [])
+    return tuple(flips)
 
 
 def findMaxIndex(a, hi):
@@ -23,15 +32,43 @@ def pancakeSort(a):
     sorted = 0
     while sorted < len(a):
         maxIndex = findMaxIndex(a, len(a) - 1 - sorted)
+        while maxIndex == len(a) - 1 - sorted and sorted != len(a) - 1:
+            sorted += 1
+            maxIndex = findMaxIndex(a, len(a) - 1 - sorted)
         flip(a, maxIndex, flips)
         flip(a, len(a) - 1 - sorted, flips)
         sorted += 1
     return flips
 
 
+def sortTuple(a):
+    srt = list(a)
+    srt.sort()
+    return tuple(srt)
+
+
+def pancakeSort2(a):
+    q = deque()
+    visited = set()
+    startState = tuple(a)
+    goalState = sortTuple(startState)
+    visited.add(startState)
+    q.append((startState, []))
+    while q:
+        currentState, currentFlips = q.popleft()
+        for i in range(len(a)):
+            newState = flipTuple(currentState, i)
+            if newState == goalState:
+                return list(newState), currentFlips + [i]
+            if newState in visited:
+                continue
+            visited.add(newState)
+            q.append((newState, currentFlips + [i]))
+
+
 if __name__ == "__main__":
     a = [1, 3, 2, 67, 8, 23, 5, 234]
     # a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     # a = [3, 2, 4, 1]
-    print(len(pancakeSort(a)))
-    # print(a)
+    # print(len(pancakeSort(a)))
+    print(pancakeSort2(a))
