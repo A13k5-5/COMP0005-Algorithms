b60051fc6614eb1e5f783fd4393a5e59b51f2e6a
A13k5-5 <pisonalex@gmail.com>
2025-02-05 11:20:32 +0100

    Advanced pancake sorting done using bfs algorithm

---
+++ labs/lab4_Sorting2/AdvancedPancakeFlipping.py
@@ -0,0 +1,47 @@
+from PancakeSorting import flipTuple, sortTuple
+
+from collections import deque
+
+
+def advancedFlip(a, start, end):
+    if start >= end:
+        return
+    m, n = start, end
+    while m < n:
+        a[m], a[n] = a[n], a[m]
+        m += 1
+        n -= 1
+
+
+def advancedFlipTuple(a, start, end):
+    l = list(a)
+    advancedFlip(l, start, end)
+    return tuple(l)
+
+
+def advancedPancakeSort(a):
+    q = deque()
+    visited = set()
+    startState = tuple(a)
+    goalState = sortTuple(startState)
+    visited.add(startState)
+    q.append((startState, []))
+    while q:
+        curState, curFlips = q.popleft()
+        for i in range(len(curState)):
+            for j in range(0, i):
+                newState = advancedFlipTuple(curState, j, i)
+                if newState == goalState:
+                    return list(newState), curFlips + [(j, i)]
+                if newState in visited:
+                    continue
+                visited.add(newState)
+                q.append((newState, curFlips + [(j, i)]))
+
+
+if __name__ == "__main__":
+    a = [1, 3, 2, 67, 8, 23, 5, 234]
+    # a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # a = [3, 2, 4, 1]
+    a = [1, 8, 9, 3, 2, 7, 6, 5, 4, 10]
+    print(advancedPancakeSort(a))
