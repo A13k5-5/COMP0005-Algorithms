ad47c24d878f7fffc2df79cd3fce749c4f91f80f
A13k5-5 <pisonalex@gmail.com>
2025-02-26 10:30:34 +0100

    Proper matrix addition

--- labs/lab5_searching/SparseMatrix.py
+++ labs/lab5_searching/SparseMatrix.py
@@ -9,10 +9,22 @@ class Matrix:
             self.rows[row] = {col: val}
         else:
             self.rows[row][col] = val
-        if row > self.height:
-            self.height = row
-        if col > self.width:
-            self.width = col
+        if row >= self.height:
+            self.height = row + 1
+        if col >= self.width:
+            self.width = col + 1
+
+    def __str__(self):
+        rows = []
+        for y in range(self.height):
+            row = []
+            for x in range(self.width):
+                if y in self.rows and x in self.rows[y]:
+                    row.append(str(self.rows[y][x]))
+                else:
+                    row.append("0")
+            rows.append(" ".join(row))
+        return "\n".join(rows)
 
     def get(self, row, col):
         if row not in self.rows.keys() or col not in self.rows[row].keys():
@@ -20,16 +32,33 @@ class Matrix:
         return self.rows[row][col]
 
     def __add__(self, mtrx2):
-        if self.height != mtrx2.height and self.width != mtrx2.height:
+        if self.height != mtrx2.height or self.width != mtrx2.width:
             raise Exception("Matrices not of same dimension")
-        newMtrx = Matrix()
+
+        result = Matrix()
+
+        for row in self.rows:
+            for col in self.rows[row]:
+                result.add_val(self.rows[row][col], row, col)
+
+        for row in mtrx2.rows:
+            for col in mtrx2.rows[row]:
+                current = result.get(row, col)
+                result.add_val(current + mtrx2.rows[row][col], row, col)
+
+        return result
 
 
 if __name__ == "__main__":
     mtrx = Matrix()
-    mtrx.add_val(5, 0, 0)
     mtrx.add_val(5, 0, 1)
-    mtrx.add_val(5, 5, 1)
-    print(mtrx.rows)
-    print(mtrx.get(0, 2))
-    print(mtrx.width, mtrx.height)
+    mtrx.add_val(1, 1, 1)
+    print(mtrx)
+
+    mtrx2 = Matrix()
+    mtrx2.add_val(1, 0, 0)
+    mtrx2.add_val(2, 1, 1)
+    print()
+    print(mtrx2)
+    print()
+    print(mtrx + mtrx2)
