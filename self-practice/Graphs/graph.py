4fed9a34ecd1214b13898ac6856702e216228746
alex <pisonalex@gmail.com>
2025-04-08 17:16:05 +0200

    DFS class implemented with edgeTo functionality

--- self-practice/Graphs/graph.py
+++ self-practice/Graphs/graph.py
@@ -4,10 +4,10 @@ class Graph:
         self.V: int = V
         self.adj = {vertex: [] for vertex in range(V)}
 
-    def V(self) -> int:
+    def getV(self) -> int:
         return self.V
 
-    def E(self) -> int:
+    def getE(self) -> int:
         return self.E
 
     def addEdge(self, v: int, w: int):
@@ -15,7 +15,7 @@ class Graph:
         self.adj[w].append(v)
         self.E += 1
 
-    def adj(self, v: int) -> list[int]:
+    def getAdj(self, v: int) -> list[int]:
         return self.adj[v]
 
     def __repr__(self):
@@ -23,6 +23,36 @@ class Graph:
         for node in self.adj:
             s += str(f"{node}: {self.adj[node]}\n")
         return s
+
+
+class DepthFirstSearch:
+    def __init__(self, g: Graph, s: int):
+        self.edgeTo = [None for _ in range(g.getV())]
+        self.marked = [False for _ in range(g.getV())]
+        self.s = s
+        self.dfs(g, s)
+
+    def dfs(self, g: Graph, v: int):
+        self.marked[v] = True
+        for w in g.getAdj(v):
+            if self.marked[w]:
+                continue
+            self.edgeTo[w] = v
+            self.dfs(g, w)
+
+    def hasPathTo(self, v: int):
+        return self.marked[v]
+
+    def pathTo(self, v):
+        if not self.hasPathTo(v):
+            return None
+        path = []
+        x = v
+        while x != self.s:
+            path.append(x)
+            x = self.edgeTo[x]
+        path.append(self.s)
+        return path
 
 
 if __name__ == "__main__":
@@ -40,4 +70,7 @@ if __name__ == "__main__":
     g.addEdge(9, 12)
     g.addEdge(9, 11)
     g.addEdge(11, 12)
-    print(g)
+    # print(g)
+    d: DepthFirstSearch = DepthFirstSearch(g, 0)
+    print(d.pathTo(5))
+    print(d.marked)
