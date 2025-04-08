33f87b12b232f78b5e0ef8e01d54d05797ea143a
alex <pisonalex@gmail.com>
2025-04-08 14:18:13 +0200

    Initialised graph API

---
+++ self-practice/Graphs/graph.py
@@ -0,0 +1,18 @@
+class Graph:
+    def __init__(self):
+        pass
+
+    def V(self) -> int:
+        pass
+
+    def E(self) -> int:
+        pass
+
+    def addEdge(self, v: int, w: int):
+        pass
+
+    def adj(self, v: int) -> list[int]:
+        pass
+
+    def __repr__(self):
+        pass
