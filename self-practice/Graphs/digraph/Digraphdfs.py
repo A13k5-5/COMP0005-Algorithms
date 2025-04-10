009ea2c5d7f3009e7a00c2d39653aba835dbcccb
alex <pisonalex@gmail.com>
2025-04-10 15:40:44 +0200

    implemented digraph dfs with a pathTo method

--- self-practice/Graphs/digraph/Digraphdfs.py
+++ self-practice/Graphs/digraph/Digraphdfs.py
@@ -3,13 +3,13 @@ from digraph import sample_digraph
 
 
 class DigraphDfs:
-    def __init__(self, g: Digraph):
+    def __init__(self, g: Digraph, s: int):
         self.marked = [False for _ in range(g.getV())]
         self.edgeTo = [-1 for _ in range(g.getV())]
+        self._dfs(g, s)
+        self.s = s
 
     def _dfs(self, g: Digraph, v: int):
-        if self.marked[v]:
-            return
         self.marked[v] = True
         for w in g.getAdj(v):
             if self.marked[w]:
@@ -17,7 +17,22 @@ class DigraphDfs:
             self.edgeTo[w] = v
             self._dfs(g, w)
 
+    def pathTo(self, v: int):
+        path = []
+        if not self.marked[v]:
+            return path
+        path.append(v)
+        x: int = self.edgeTo[v]
+        while x != self.s:
+            path.append(x)
+            x = self.edgeTo[x]
+        path.append(self.s)
+        return path
+
 
 if __name__ == "__main__":
     d: Digraph = sample_digraph()
     print(d)
+    dfs: DigraphDfs = DigraphDfs(d, 0)
+    print(dfs.marked)
+    print(dfs.pathTo(3))
