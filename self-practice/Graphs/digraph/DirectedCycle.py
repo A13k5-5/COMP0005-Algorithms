8d472468665c86f7f19906c4d51562dae3f24eb3
alex <pisonalex@gmail.com>
2025-04-10 16:38:48 +0200

    Implemented hasCycle fuctionality

---
+++ self-practice/Graphs/digraph/DirectedCycle.py
@@ -0,0 +1,29 @@
+from digraph import Digraph, sample_digraph
+
+
+class DirectedCycle:
+    def __init__(self, g: Digraph):
+        self.onStack = [False for _ in range(g.getV())]
+        self.marked = [False for _ in range(g.getV())]
+        self.hasCycle = False
+        for s in range(g.getV()):
+            self._dfs(g, s)
+
+    def _dfs(self, g: Digraph, v: int):
+        self.marked[v] = True
+        self.onStack[v] = True
+        for w in g.getAdj(v):
+            if self.hasCycle:
+                return
+            self.marked[w] = True
+            if self.onStack[w]:
+                self.hasCycle = True
+                continue
+            self._dfs(g, w)
+        self.onStack[v] = False
+
+
+if __name__ == "__main__":
+    g: Digraph = sample_digraph()
+    d: DirectedCycle = DirectedCycle(g)
+    print(d.hasCycle)
