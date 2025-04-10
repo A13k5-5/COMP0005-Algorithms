970d34d990b41393e36b718468b51c0fd638d36d
alex <pisonalex@gmail.com>
2025-04-10 16:56:02 +0200

    Returns the cycle in a digraph

--- self-practice/Graphs/digraph/DirectedCycle.py
+++ self-practice/Graphs/digraph/DirectedCycle.py
@@ -5,7 +5,8 @@ class DirectedCycle:
     def __init__(self, g: Digraph):
         self.onStack = [False for _ in range(g.getV())]
         self.marked = [False for _ in range(g.getV())]
-        self.hasCycle = False
+        self.edgeTo = [-1 for _ in range(g.getV())]
+        self.cycle = None
         for s in range(g.getV()):
             if not self.marked[s]:
                 self._dfs(g, s)
@@ -14,16 +15,28 @@ class DirectedCycle:
         self.marked[v] = True
         self.onStack[v] = True
         for w in g.getAdj(v):
-            if self.hasCycle:
+            if self.hasCycle():
                 return
             elif not self.marked[w]:
+                self.edgeTo[w] = v
                 self._dfs(g, w)
             elif self.onStack[w]:
-                self.hasCycle = True
+                self.cycle = []
+                x: int = v
+                while x != w:
+                    self.cycle.append(x)
+                    x = self.edgeTo[x]
+                self.cycle.append(w)
+                self.cycle.append(v)
+                self.cycle = self.cycle[::-1]
+
         self.onStack[v] = False
+
+    def hasCycle(self):
+        return self.cycle is not None
 
 
 if __name__ == "__main__":
     g: Digraph = sample_digraph()
     d: DirectedCycle = DirectedCycle(g)
-    print(d.hasCycle)
+    print(d.cycle)
