fc089a6319adb6d890fe9d4f0c6d6378c33d4746
alex <pisonalex@gmail.com>
2025-04-10 19:43:45 +0200

    Post order and reverse post order calculated in a reparate class

---
+++ self-practice/Graphs/digraph/DepthFirstOrder.py
@@ -0,0 +1,25 @@
+from digraph import Digraph
+
+
+class DepthFirstOrder:
+    def __init__(self, g: Digraph):
+        self.marked = [False for _ in range(g.getV())]
+        self.post = []
+        for s in range(g.getV()):
+            if self.marked[s]:
+                continue
+            self._dfs(g, s)
+
+    def _dfs(self, g: Digraph, v: int):
+        self.marked[v] = True
+        for w in g.getAdj(v):
+            if self.marked[w]:
+                continue
+            self._dfs(g, w)
+        self.post.append(v)
+
+    def getReversePost(self):
+        return self.post[::-1]
+
+    def getPost(self):
+        return self.post
