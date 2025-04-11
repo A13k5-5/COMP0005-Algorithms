a8736bfbfaa6d752454d99635577e9e0aad97f2f
alex <pisonalex@gmail.com>
2025-04-11 12:37:09 +0200

    LazyPrimMST api initialised

---
+++ self-practice/Graphs/EdgeWeightedGraphs/LazyPrimsMST.py
@@ -0,0 +1,13 @@
+from EdgeWeightedGraph import EdgeWeightedGraph, loadEWG
+from Edge import Edge
+
+
+class MST:
+    def __init__(self, g: EdgeWeightedGraph):
+        pass
+
+    def getEdes(self):
+        pass
+
+    def getWeight(self):
+        pass
