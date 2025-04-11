908b3a68a3db56084ce416472a217870eeac4d28
alex <pisonalex@gmail.com>
2025-04-11 11:12:32 +0200

    Working edge class that implements comparison operators

---
+++ self-practice/Graphs/EdgeWeightedGraphs/Edge.py
@@ -0,0 +1,27 @@
+class Edge:
+    def __init__(self, v: int, w: int, weight: float):
+        self.v: int = v
+        self.w: int = w
+        self.weight = weight
+
+    def either(self):
+        return self.v
+
+    def other(self, vertex: int):
+        if vertex == self.v:
+            return self.w
+        elif vertex == self.w:
+            return self.v
+        raise Exception("Illegal argument")
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __eq__(self, other):
+        return self.weight == other.weight
+
+
+if __name__ == "__main__":
+    e1: Edge = Edge(1, 2, 7)
+    e2: Edge = Edge(1, 2, 7)
+    print(e1 == e2)
