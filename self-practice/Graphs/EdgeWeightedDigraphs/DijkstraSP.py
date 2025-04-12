be84df878ebac0b5a940a5bed2f404614b03ffc9
alex <pisonalex@gmail.com>
2025-04-12 15:55:46 +0200

    Dijkstras algorithm implemented

--- self-practice/Graphs/EdgeWeightedDigraphs/DijkstraSP.py
+++ self-practice/Graphs/EdgeWeightedDigraphs/DijkstraSP.py
@@ -1,12 +1,56 @@
+import heapq
+
 from DirectedEdge import DirectedEdge
-from EdgeWeightedDigraph import EdgeWeightedDigraph
+from EdgeWeightedDigraph import EdgeWeightedDigraph, loadEWD
 
 
 class DijkstraSP:
-    def __int__(self, g: EdgeWeightedDigraph, s: int):
-        self.distTo: list[float] = [float("int") for _ in range(g.getV())]
+    def __init__(self, g: EdgeWeightedDigraph, s: int):
+        self.distTo: list[float] = [float("inf") for _ in range(g.getV())]
+        self.distTo[s] = 0.0
         self.edgeTo = [None for _ in range(g.getV())]
-        self.pq: list[DirectedEdge] = []
+        self.pq = []
+        heapq.heappush(self.pq, (self.distTo[s], s))
 
-    def relax(self):
-        pass
+        while len(self.pq) > 0:
+            dist, v = heapq.heappop(self.pq)
+            if dist > self.distTo[v]:
+                continue
+            for e in g.getAdj(v):
+                self.relax(e)
+
+    def relax(self, e: DirectedEdge):
+        v: int = e.getSource()
+        w: int = e.getDestination()
+        if self.distTo[w] > self.distTo[v] + e.getWeight():
+            self.distTo[w] = self.distTo[v] + e.getWeight()
+            self.edgeTo[w] = e
+            heapq.heappush(self.pq, (self.distTo[w], w))
+
+    def hasPathTo(self, v: int):
+        return self.distTo[v] < float("inf")
+
+    def getDistTo(self, v: int):
+        return self.distTo[v]
+
+    def pathTo(self, v: int):
+        if not self.hasPathTo(v):
+            return None
+        path = []
+        e = self.edgeTo[v]
+        while e is not None:
+            path.append(e)
+            e = self.edgeTo[e.getSource()]
+        path.reverse()
+        return path
+
+
+if __name__ == "__main__":
+    g: EdgeWeightedDigraph = loadEWD("tinyEWD.txt")
+    s = 0
+    sp = DijkstraSP(g, s)
+    for v in range(g.getV()):
+        print(f"Distance to {v}: {sp.getDistTo(v)}")
+        if sp.hasPathTo(v):
+            print(f"Path: {sp.pathTo(v)}")
+        print()
