5e6fb7046abb3858a4f456e2c187f996a4cc2201
alex <pisonalex@gmail.com>
2025-04-12 13:18:36 +0200

    Binary Min Heap implemented and tested

--- self-practice/Sorting/PriorityQueue/MinPQ.py
+++ self-practice/Sorting/PriorityQueue/MinPQ.py
@@ -7,22 +7,47 @@ class MinPQ:
         return self.n == 0
 
     def insert(self, v):
-        pass
+        self.pq.append(v)
+        self.n += 1
+        self.swim(self.n)
+
+    def delMin(self):
+        if self.n < 1:
+            raise Exception("Empty heap")
+        min = self.pq[1]
+        self.exch(1, self.n)
+        self.n -= 1
+        self.sink(1)
+        return min
 
     def sink(self, k: int):
         while 2 * k <= self.n:
-            j = 2 * k
-            if j < self.n and self.isMore(j, j + 1):
-                j += 1
-            if not self.isMore(j, k):
-                self.exch(j, k)
-            k = j
+            # Left child
+            c = 2 * k
+            if c < self.n and self.isMore(c, c + 1):
+                c += 1
+            if not self.isMore(c, k):
+                self.exch(c, k)
+            k = c
 
     def swim(self, k: int):
-        pass
+        while k > 1 and self.isMore(k // 2, k):
+            self.exch(k // 2, k)
+            k = k // 2
 
     def exch(self, i: int, j: int):
         self.pq[i], self.pq[j] = self.pq[j], self.pq[i]
 
     def isMore(self, i: int, j: int):
         return self.pq[i] > self.pq[j]
+
+
+import random
+
+if __name__ == "__main__":
+    pq: MinPQ = MinPQ()
+    for _ in range(10):
+        pq.insert(random.randint(0, 100))
+    print(pq.pq)
+    for _ in range(pq.n):
+        print(pq.delMin())
