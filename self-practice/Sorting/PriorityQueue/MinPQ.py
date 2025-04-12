b89da4398c8710f0e5694b705545c37c23d37687
alex <pisonalex@gmail.com>
2025-04-12 12:44:20 +0200

    Implemented sink method

--- self-practice/Sorting/PriorityQueue/MinPQ.py
+++ self-practice/Sorting/PriorityQueue/MinPQ.py
@@ -1,18 +1,28 @@
 class MinPQ:
     def __init__(self):
-        pass
+        self.pq = [None]
+        self.n = 0
 
-    def insert(self, v: int):
+    def isEmpty(self):
+        return self.n == 0
+
+    def insert(self, v):
         pass
 
     def sink(self, k: int):
-        pass
+        while 2 * k <= self.n:
+            j = 2 * k
+            if j < self.n and self.isMore(j, j + 1):
+                j += 1
+            if not self.isMore(j, k):
+                self.exch(j, k)
+            k = j
 
     def swim(self, k: int):
         pass
 
-    def exch(self, i: int, k: int):
-        pass
+    def exch(self, i: int, j: int):
+        self.pq[i], self.pq[j] = self.pq[j], self.pq[i]
 
     def isMore(self, i: int, j: int):
-        pass
+        return self.pq[i] > self.pq[j]
