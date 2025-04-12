4cc978b81436d5d7c35eab53a71242447939d7a8
alex <pisonalex@gmail.com>
2025-04-12 12:36:38 +0200

    API for MinPQ

---
+++ self-practice/Sorting/PriorityQueue/MinPQ.py
@@ -0,0 +1,18 @@
+class MinPQ:
+    def __init__(self):
+        pass
+
+    def insert(self, v: int):
+        pass
+
+    def sink(self, k: int):
+        pass
+
+    def swim(self, k: int):
+        pass
+
+    def exch(self, i: int, k: int):
+        pass
+
+    def isMore(self, i: int, j: int):
+        pass
